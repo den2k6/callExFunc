9b1b41515fcc83feb275188885cbcd8c79e833c7
den2k6 <67808977+den2k6@users.noreply.github.com>
2021-09-04 08:04:08 +0900

    chaged on GitHub

--- main1.py
+++ main1.py
@@ -4,9 +4,8 @@ import subprocess
 
 def main():
     print('Hello from main1')
-    # subprocess.run(['python3', 'sub1.py'])
-    subprocess.run(['./sub1.py'])
+    subprocess.run(['python3', 'sub1.py'])
+    # subprocess.run(['./sub1.py'])
 
 if __name__ == '__main__':
     main()
-
