388a36ffb383eeb031d1132c3ff7c11443f34dd3
den.2k6 <den.2k6aug17@gmail.com>
2021-09-04 07:17:03 +0900

    initial commit

---
+++ main1.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+import subprocess
+
+def main():
+    print('Hello from main1')
+    # subprocess.run(['python3', 'sub1.py'])
+    subprocess.run(['./sub1.py'])
+
+if __name__ == '__main__':
+    main()
